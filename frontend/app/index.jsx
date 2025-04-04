e98d6fbaa85220873e48b6e224db0bd54bfb464c
NureniJamiu <nurenijamiu951@gmail.com>
2025-04-04 07:47:43 -0700

    complete backend and initialize frontend

---
+++ frontend/app/index.jsx
@@ -0,0 +1,18 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ color: "green" }}>Hello</Text>
+      <Link href="/(auth)/signup">Register </Link>
+      <Link href="/(auth)">Login </Link>
+    </View>
+  );
+}
